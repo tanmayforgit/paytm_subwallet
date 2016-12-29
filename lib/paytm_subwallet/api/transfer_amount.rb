848e1f25c4eba9080bae0e00abc24ceaeb4f7458
Anagha kelkar <aukelkar@gmail.com>
2016-12-29 11:25:06 +0530

    change of configuration for paytm

--- lib/paytm_subwallet/api/transfer_amount.rb
+++ lib/paytm_subwallet/api/transfer_amount.rb
@@ -1,18 +1,38 @@
 module PaytmSubWallet
   module Api
     class TransferAmount
-      def initialize
+      def initialize(body)
+        @body = body
       end
 
-      def all(query)
-        headers = HeadersConstructor.new(query).construct
-        HTTParty.post(httparty_url, headers: headers, query: query)
+      def call
+        headers = HeadersConstructor.new(body).construct
+        Response.new(HTTParty.post(httparty_url, headers: headers, querry: body))
       end
 
       private
 
       def httparty_url
         "http://trust-uat.paytm.in/wallet-web/salesToUserCredit"
+      end
+
+      class Response
+        def initialize(httparty_response)
+          # @request_guid = httparty_response[]
+          # .
+          # .
+          # .
+          # .
+
+        end
+
+        def success?
+          
+        end
+
+        # def some_other_case?
+          
+        # end
       end
     end
   end
