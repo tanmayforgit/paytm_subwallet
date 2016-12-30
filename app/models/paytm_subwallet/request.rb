97c9dc83de11b0d12456d602411ad1232b4f7c55
AnaghaKelkar <aukelkar@gmail.com>
2016-12-30 11:19:10 +0530

    added associations in models

--- app/models/paytm_subwallet/request.rb
+++ app/models/paytm_subwallet/request.rb
@@ -1,10 +1,12 @@
 module PaytmSubwallet
   class Request < ActiveRecord::Base
+  	belongs_to :assignable, polymorphic: true
+   	has_many :paytm_subwallet_responses, class_name: PaytmSubwallet::Response
 
-  	def self.next_merchant_order_id
-  		# generate alphanumeric(small letters + numbers) string
+    def self.next_merchant_order_id
+      # generate alphanumeric(small letters + numbers) string
       # check if some request already has it
       # retry if true return string if not
-  	end
+    end
   end
 end
--- app/models/paytm_subwallet/response.rb
+++ app/models/paytm_subwallet/response.rb
@@ -1,4 +1,6 @@
 module PaytmSubwallet
   class Response < ActiveRecord::Base
+  	belongs_to :assignable, polymorphic: true
+    belongs_to :paytm_subwallet_request, class_name: PaytmSubwallet::Request
   end
 end
